01394522bf4c85011bfb7481b88c850e427f2e41
DESKTOP-U7QOK1Q\Puesto1 <csi22-irodhan@colegioaltair.net>
2023-11-09 11:30:39 +0100

    9112023

---
+++ src/test/java/api/java/Api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package api.java.Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
